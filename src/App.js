c926d171cc103eae34982ca6f82bc82015a2221f
O S <vystoya@gmail.com>
2018-09-24 00:23:11 +0300

    download spredsheet and display it on html table

--- src/App.js
+++ src/App.js
@@ -10,7 +10,42 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/students">
+                  Students <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/exams">
+                  Exams
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/results">
+                  Results
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        {/* <nav className="navbar navbar-light bg-light">
           <a className="navbar-brand" href="">
             Students <span className="badge badge-pill badge-secondary" />
           </a>
@@ -31,7 +66,7 @@ class App extends Component {
               </a>
             </li>
           </ul>
-        </nav>
+        </nav> */}
         <Router>
           <Switch>
             <Route exact path="/exams" component={Exam} />
@@ -39,10 +74,6 @@ class App extends Component {
             <Route path="/students" component={Student} />
           </Switch>
         </Router>
-
-        <Student />
-        <Exam />
-        <Result />
       </React.Fragment>
     );
   }
