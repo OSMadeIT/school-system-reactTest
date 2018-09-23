9e2ce4d4e9fdd8cb737e1116366750a9759e4373
O S <vystoya@gmail.com>
2018-09-23 19:44:21 +0300

    implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Student from "./components/students";
 import Exam from "./components/exams";
 import Result from "./components/results";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -13,9 +14,34 @@ class App extends Component {
           <a className="navbar-brand" href="">
             Students <span className="badge badge-pill badge-secondary" />
           </a>
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="/students">
+                Students <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/exams">
+                Exams
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/results">
+                Results
+              </a>
+            </li>
+          </ul>
         </nav>
+        <Router>
+          <Switch>
+            <Route exact path="/exams" component={Exam} />
+            <Route path="/results" component={Result} />
+            <Route path="/students" component={Student} />
+          </Switch>
+        </Router>
+
         <Student />
-        {/* <Exam /> */}
+        <Exam />
         <Result />
       </React.Fragment>
     );
